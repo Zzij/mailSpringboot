e45733e07e22c8c4a6ebd86f4969ef7a14ec4fa9
zzj <1241646494@qq.com>
2018-10-25 17:31:41 +0800

    main

--- src/main/java/com/zz/helloWorld/MailApplication.java
+++ src/main/java/com/zz/helloWorld/MailApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MailApplication {
 
+	//启动项
 	public static void main(String[] args) {
 		SpringApplication.run(MailApplication.class, args);
 	}
